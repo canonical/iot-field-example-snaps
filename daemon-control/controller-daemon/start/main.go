c52906ddfe489d7248fc56408f64ab34344969f2
Kyle Nitzsche <kyle.nitzsche@canonical.com>
2020-02-20 18:18:42 +0000

    Add stop command and some cleanups

--- daemon-control/controller-daemon/start/main.go
+++ daemon-control/controller-daemon/start/main.go
@@ -9,16 +9,27 @@ import (
 )
 
 func main() {
-	snapDaemon := "test-controlled-daemons.controlled-1"
+	snapDaemon1 := "test-controller-daemons.controlled-1"
+	snapDaemon2 := "test-controlled-daemons.controlled-1"
 	for { // loop forever
 		time.Sleep(20 * time.Second)
-		fmt.Printf("About to start %s\n", snapDaemon) 
 		snap := snapdapi.NewClientAdapter()
 		startOpts := client.StartOptions{}
 		startOpts.Enable = false
 		startMe := make([]string, 1)
-		startMe[0] = snapDaemon
+		//start first daemon
+		fmt.Printf("About to start %s\n", snapDaemon1)
+		startMe[0] = snapDaemon1
 		ID, err2:= snap.Start(startMe, startOpts)
+		if err2 != nil{
+			fmt.Println(err2)
+		} else {
+			fmt.Printf("Change ID :%s\n", ID)
+		}
+		//start second daemon
+		fmt.Printf("About to start %s\n", snapDaemon1)
+		startMe[0] = snapDaemon2
+		ID, err2 = snap.Start(startMe, startOpts)
 		if err2 != nil{
 			fmt.Println(err2)
 		} else {
