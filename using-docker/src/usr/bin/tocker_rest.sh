757f7991982545d65314ac67c1070a7ebce7e304
Dilyn Corner <dilyn.corner@canonical.com>
2023-05-10 10:34:49 -0400

    tocker_rest.sh: fix Docker REST API URLs
    Signed-off-by: Dilyn Corner <dilyn.corner@canonical.com>

--- using-docker/src/usr/bin/tocker_rest.sh
+++ using-docker/src/usr/bin/tocker_rest.sh
@@ -2,21 +2,21 @@
 
 image() {
     curl --unix-socket /run/docker.sock -X POST \
-        http://v1.41/images/create?fromImage=nginx:latest
+        http://localhost/v1.41/images/create?fromImage=nginx:latest
 }
 
 list() {
-    curl --unix-socket /run/docker.sock -X GET http://v1.41/containers/json
+    curl --unix-socket /run/docker.sock -X GET http://localhost/v1.41/containers/json
 }
 
 server() {
     curl --unix-socket /run/docker.sock -X POST    \
         -H "Content-Type: application/json"        \
         -d "@$SNAP/usr/share/composers/nginx.json" \
-        http://v1.41/containers/create?name=nginx-server
+        http://localhost/v1.41/containers/create?name=nginx-server
 }
 
 start() {
     curl --unix-socket /run/docker.sock -X POST \
-        http://v1.41/containers/nginx-server/start
+        http://localhost/v1.41/containers/nginx-server/start
 }
